03aabc8dee8a5a9b70e5835eaab2deb113aada82
Torpak <68283117+torpack@users.noreply.github.com>
2024-10-03 18:58:13 +0300

    Update config.lua
    Framework Support Added

--- config.lua
+++ config.lua
@@ -2,6 +2,10 @@ Config = {}
 
 Config.Distance = 50
 
+Config.DisplayName = true -- If you are using ESX or QBCore
+Config.Framework = "QBCore" -- QBCore or ESX
+Config.Surname = false -- true if you want display surname too
+
 Config.Admins = { -- Admin list for tage
     'Steam Hex',
     'Steam Hex'
